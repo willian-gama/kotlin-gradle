82f72fab605d9cf4dc949ddb2ba6605bff7459df
williangama <>
2024-04-05 13:36:31 +0100

    remove unused classes

--- src/main/kotlin/com/willian/gama/constants/ExtensionConstants.kt
+++
@@ -1,5 +0,0 @@
-package com.willian.gama.constants
-
-object ExtensionConstants {
-    const val CODE_ANALYSIS_TOOL = "code_analysis_tool"
-}
--- src/main/kotlin/com/willian/gama/model/CodeAnalysisParams.kt
+++
@@ -1,7 +0,0 @@
-package com.willian.gama.model
-
-open class CodeAnalysisParams {
-    lateinit var sonarProjectKey: String
-    lateinit var sonarProjectVersion: String
-    lateinit var sonarToken: String
-}
