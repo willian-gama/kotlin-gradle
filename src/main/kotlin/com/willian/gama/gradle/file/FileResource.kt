4fd70a5f49c32e74b0449d776d41a96b83a64b2e
williangama <>
2024-04-06 14:57:12 +0100

    remove unused code

--- src/main/kotlin/com/willian/gama/gradle/file/FileResource.kt
+++ src/main/kotlin/com/willian/gama/gradle/file/FileResource.kt
@@ -1,43 +1,16 @@
 package com.willian.gama.gradle.file
 
 import java.io.File
-import java.io.InputStream
-
-private val DETEKT_CONFIG_FILE by lazy {
-    File("config", ".yml")
-}
 
 object FileResource {
+    private val DETEKT_FILE_CONFIG by lazy { File.createTempFile("config", ".yml") }
+
     fun getFileFromResource(fileName: String): File {
         return requireNotNull(javaClass.classLoader.getResource(fileName)).openStream().use {
-            val file: File = File.createTempFile("config", ".yml")
-            file.outputStream().use { output ->
+            DETEKT_FILE_CONFIG.outputStream().use { output ->
                 it.copyTo(output)
             }
-            file
+            DETEKT_FILE_CONFIG
         }
     }
-
-    fun getDetektConfigFile(inputStream: InputStream?): File {
-        if (DETEKT_CONFIG_FILE.length() == 0L) {
-            requireNotNull(inputStream).use {
-                DETEKT_CONFIG_FILE.outputStream().use { output ->
-                    it.copyTo(output)
-                }
-            }
-        }
-        println(DETEKT_CONFIG_FILE.name)
-        return DETEKT_CONFIG_FILE
-    }
-
-//    private val DETEKT_CONFIG_FILE by lazy {
-//        File("detekt.yml").run {
-//            CodeAnalysisPlugin::class.java.javaClass.classLoader.getResourceAsStream("linting/detekt/detekt.yml")!!
-//                .use {
-//                    outputStream().use { output ->
-//                        it.copyTo(output)
-//                    }
-//                }
-//        }
-//    }
 }
