e1a76b79035bdd9cb274a94769399402118efcb3
willian-gama <wgama@kitmanlabs.com>
2024-08-30 13:10:50 +0100

    update code

--- scripts/auto_sync_prs.sh
+++ scripts/auto_sync_prs.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
 TARGET_BRANCH=$1
-
-#if [ -z "$(git config --get user.name)" ]; then
-#  git config user.name "renovate[bot]"
-#fi
-#
-#if [ -z "$(git config --get user.email)" ]; then
-#  git config user.email "29139614+renovate[bot]@users.noreply.github.com"
-#fi
-
 PR_BRANCHES=$(gh pr list --state open --base "$TARGET_BRANCH" --json headRefName --jq '.[].headRefName')
 
 echo "Target branch: $TARGET_BRANCH"
@@ -18,6 +9,6 @@ for branch in $PR_BRANCHES; do
   echo "Syncing $branch"
   git fetch origin "$branch"
   git checkout "$branch"
-  git merge origin/"$TARGET_BRANCH" --no-edit
+  git merge "$TARGET_BRANCH" --no-edit
   git push origin "$branch"
 done
