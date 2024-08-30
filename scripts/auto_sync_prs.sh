f492d678156d04778f82c87d1b9ec2313bab3bc2
willian-gama <wgama@kitmanlabs.com>
2024-08-30 18:29:26 +0100

    test

--- scripts/auto_sync_prs.sh
+++ scripts/auto_sync_prs.sh
@@ -4,13 +4,13 @@ GH_BRANCH=dev
 # https://cli.github.com/manual/gh_pr_view
 PR_BRANCHES=$(gh pr list --state open --base "$GH_BRANCH" --json headRefName --jq '.[].headRefName')
 
-if [ -z "$(git config --get user.name)" ]; then
-  git config user.name "github-actions[bot]"
-fi
-
-if [ -z "$(git config --get user.email)" ]; then
-  git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
-fi
+#if [ -z "$(git config --get user.name)" ]; then
+#  git config user.name "github-actions[bot]"
+#fi
+#
+#if [ -z "$(git config --get user.email)" ]; then
+#  git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+#fi
 
 for branch in $PR_BRANCHES; do
   echo -e "\nSyncing $branch\n"
