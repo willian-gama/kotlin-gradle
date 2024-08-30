aae15e9a384ccef84737155bcbad3ad6a1542d3f
willian-gama <wgama@kitmanlabs.com>
2024-08-30 12:52:01 +0100

    update script

--- scripts/auto_sync_prs.sh
+++ scripts/auto_sync_prs.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-TARGET_BRANCH=develop
-
 if [ -z "$(git config --get user.name)" ]; then
   git config user.name "renovate[bot]"
 fi
@@ -10,15 +8,11 @@ if [ -z "$(git config --get user.email)" ]; then
   git config user.email "29139614+renovate[bot]@users.noreply.github.com"
 fi
 
-git fetch --all
-
-PR_BRANCHES=$(gh pr list --state open --repo willian-gama/kgp)
-
-echo "$PR_BRANCHES"
+PR_BRANCHES=$(gh pr list --state open --base "$TARGET_BRANCH" --json headRefName --jq '.[].headRefName')
 
 for branch in $PR_BRANCHES; do
-  echo "$branch"
-#  git checkout "$branch"
-#  git merge origin/"$TARGET_BRANCH" --no-edit
-#  git push origin "$branch"
+  git fetch origin "$branch"
+  git checkout "$branch"
+  git merge origin/"$TARGET_BRANCH" --no-edit
+  git push origin "$branch"
 done
