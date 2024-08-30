abe6ae38ee6e14a016483295c11a0e8fb40ae620
willian-gama <wgama@kitmanlabs.com>
2024-08-30 19:14:18 +0100

    test

--- scripts/auto_sync_prs.sh
+++ scripts/auto_sync_prs.sh
@@ -14,14 +14,14 @@ fi
 
 if [ ${#PR_BRANCHES[@]} -gt 0 ]; then
   for branch in $PR_BRANCHES; do
-    echo -e "\nSyncing $branch\n"
-
     git fetch origin "$branch"
     git checkout "$branch"
 
-    if git merge "origin/$GIT_BRANCH" --no-edit 2>/dev/null; then
+    if git merge "origin/$GIT_BRANCH" --no-edit 2>&1; then
+      echo -e "\nSyncing $branch\n with $GIT_BRANCH"
       git push origin "$branch"
     else
+      echo -e "\nThis branch: $branch has conflicts that must be resolved"
       git merge --abort
     fi
   done
