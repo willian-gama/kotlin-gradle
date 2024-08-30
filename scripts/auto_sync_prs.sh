cf3a6c51b873d0b3595f799b75aede0345f7921d
Willian Gama <10974743+willian-gama@users.noreply.github.com>
2024-08-30 18:34:33 +0100

    update auto sync prs script

--- scripts/auto_sync_prs.sh
+++ scripts/auto_sync_prs.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-GH_BRANCH=$1
-# https://cli.github.com/manual/gh_pr_view
-PR_BRANCHES=$(gh pr list --state open --base "$GH_BRANCH" --json headRefName --jq '.[].headRefName')
+GIT_BRANCH=$CIRCLE_BRANCH
 
-echo "Fetching target branch: $GH_BRANCH"
-git fetch origin "$GH_BRANCH"
+# https://cli.github.com/manual/gh_pr_view
+PR_BRANCHES=$(gh pr list --state open --base "$GIT_BRANCH" --json headRefName --jq '.[].headRefName')
+
+if [ -z "$(git config --get user.name)" ]; then
+  git config user.name "github-actions[bot]"
+fi
+
+if [ -z "$(git config --get user.email)" ]; then
+  git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+fi
 
 for branch in $PR_BRANCHES; do
-  echo "Syncing $branch"
+  echo -e "\nSyncing $branch\n"
+
   git fetch origin "$branch"
   git checkout "$branch"
-  git merge "origin/$GH_BRANCH" --no-edit
+
+  if ! git merge "origin/$GIT_BRANCH" --no-edit; then
+    git merge --abort
+    continue
+  fi
+
   git push origin "$branch"
 done
