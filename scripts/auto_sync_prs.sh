ebde902cbdd35612387888020213607d91cfb7d6
willian-gama <wgama@kitmanlabs.com>
2024-09-02 18:11:20 +0100

    update auto sync pr script

--- scripts/auto_sync_prs.sh
+++ scripts/auto_sync_prs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 GIT_BRANCH=$CIRCLE_BRANCH
-# https://cli.github.com/manual/gh_pr_view
-PR_BRANCHES=$(gh pr list --state open --base "$GIT_BRANCH" --json headRefName --jq '.[].headRefName')
+# Github client: https://cli.github.com/manual/gh_pr_view
+PR_BRANCHES=$(gh pr list --state open --base "$GIT_BRANCH" --json headRefName,isDraft --jq ".[] | select(.isDraft == false) | .headRefName")
 
 if [ -z "$(git config --get user.name)" ]; then
   git config user.name "github-actions[bot]"
@@ -12,11 +12,10 @@ if [ -z "$(git config --get user.email)" ]; then
   git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
 fi
 
-if [ ${#PR_BRANCHES[@]} -gt 0 ]; then
+if [ -n "$PR_BRANCHES" ]; then
   for branch in $PR_BRANCHES; do
     echo -e "\n- Syncing branches: $branch with $GIT_BRANCH\n"
 
-    git fetch origin "$branch"
     git checkout "$branch"
 
     if git merge "origin/$GIT_BRANCH" --no-edit > /dev/null 2>&1; then
@@ -28,5 +27,5 @@ if [ ${#PR_BRANCHES[@]} -gt 0 ]; then
     fi
   done
 else
-  echo "No PRs available for the target: $GIT_BRANCH"
+  echo "No PRs available for the target branch: $GIT_BRANCH"
 fi
