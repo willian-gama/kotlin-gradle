141bc3f6b9d54487942e326ba62a411193b092dc
willian-gama <wgama@kitmanlabs.com>
2024-10-03 17:07:33 +0100

    simplify script

--- scripts/copy_ktlint_rules.sh
+++ scripts/copy_ktlint_rules.sh
@@ -4,12 +4,25 @@
 KOTLIN_FILE=src/main/kotlin/com/kitmanlabs/code/analysis/constants/KtLintConstants.kt
 EDITORCONFIG_FILE=".editorconfig"
 
+if [ ! -f "$KOTLIN_FILE" ]; then
+  echo "File not found $KOTLIN_FILE"
+  exit 1
+fi
+
+rules=$(sed -n '/KTLINT_RULES *=/,/^)/p' "$KOTLIN_FILE" | grep -E 'to' | sed 's/"//g; s/,//; s/)$//')
+
+if [[ -z "$rules" ]]; then
+  echo "Error: No KTLINT rules found in '$KOTLIN_FILE'!"
+  exit 1
+fi
+
 echo "[*.{kt,kts}]" > "$EDITORCONFIG_FILE"
 
-sed -n '/KTLINT_RULES *=/,/^)/p' "$KOTLIN_FILE" | grep -E 'to' | while IFS=' ' read -r rule _ status; do
+echo "$rules" | while IFS=' ' read -r rule _ status; do
   rule=$(echo "$rule" | sed 's/"//g; s/,//')
   status=$(echo "$status" | sed 's/"//g; s/,//; s/)$//')
   echo "$rule = $status" >> "$EDITORCONFIG_FILE"
 done
 
-echo "Created .editorconfig from $KOTLIN_FILE"
+echo "$EDITORCONFIG_FILE created from $KOTLIN_FILE"
+cat "$EDITORCONFIG_FILE"
